09d307ec110fd79be547abe976c76d616aeb5f9e
Igor Smorodsky <ingerard@gmail.com>
2022-06-29 13:09:27 +0300

    trying add comps

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'normalize.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ShoppingList from './components/shoplist';
+import DataTable from './components/list';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <ShoppingList />
+    <DataTable />
   </React.StrictMode>
 );
 
